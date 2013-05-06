f30cc78febd133446bb2ebe99de93d0af3f64f9a
Jeffrey Kao <jeffthestampede@gmail.com>
2013-05-06 16:49:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cssdash::Application.config.secret_token = 'bbdde61ab856342003fe9ef1d4ccda58660959b43f65e7adb9ae71a7a5d53780eb883d76d99e7b8357b2b1e6df8608b47c6763a870b23b704d744a853b6afc27'
