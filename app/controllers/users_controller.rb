89b45303f92df4151e4635d3c92b07ba33a71f9c
Michelle Lowe <mick_571@yahoo.com>
2013-05-08 00:22:51 -0700

    Moved some code from views to users_controller, displaying content contributed on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
-class UsersController < ActionController::Base
+class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @contents = @user.contents
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @user }
+    end
   end
 
 end
