6a210104380a0497f49ae416f94307f1bc57194e
Michelle Lowe <mick_571@yahoo.com>
2013-05-08 19:05:42 -0700

    Added act_as_taggable_on gem

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,5 +1,6 @@
 class Content < ActiveRecord::Base
-  attr_accessible :created_on, :css, :deleted, :description, :html, :title, :visible, :likes, :views, :favorites, :user_id
+  attr_accessible :created_on, :css, :deleted, :description, :html, :title, :visible, :likes, :views, :favorites, :user_id, :tag_list
+  acts_as_taggable
   belongs_to :user
   has_many :content_tags
   has_many :comments
