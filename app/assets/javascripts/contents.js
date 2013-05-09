a538180f5875460facd7c6f308dd4198f24de180
Jeffrey Kao <jeffthestampede@gmail.com>
2013-05-09 15:33:01 -0700

    added live rendering to home index page

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -5,7 +5,8 @@ $(document).ready( function() {
   function htmlrender() {
     var html = $("#htmloutput").val();
     var css = $("#cssoutput").val();
-    var iframe = $("#iframe");
+    var children = $('#iframe-wrapper').children();
+    var iframe = $(children[0]);
     iframe.appendTo('#iframe').contents().find('body').html(html);
     iframe.appendTo('#iframe').contents().find('head').html('<style>' + css + '</style>');
   }
