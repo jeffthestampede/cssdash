207c0a6b8cd1c2bde96f0f14ce9c5e540a57c804
Jeffrey Kao <jeffthestampede@gmail.com>
2013-05-10 16:10:44 -0700

    test wooj.jpg default avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def avatar_url(user)
-    default_url = image_url("gravatar_default.svg")
+    default_url = image_url("wooj.jpeg")
     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
     # "http://gravatar.com/avatar/#{gravatar_id}.png?s=220"
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=220&d=#{CGI.escape(default_url)}"
