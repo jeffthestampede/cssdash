5163b8916c942f33e935fc4fc0a170c6f5930e5a
Livien Yin <LivienYin@gmail.com>
2013-05-10 01:05:19 -0700

    Javascript added for mouseover card_description on homepage

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  $(".card").each(function () {
+    var cardDescription = $(this).children('.card_description');
+    var iframeWrapper = $(this).children('.iframe_wrapper');
+    $(this).mouseover(function () {
+      iframeWrapper.addClass('hidden');
+      cardDescription.removeClass('hidden');
+    });
+    $(this).mouseout(function () {
+      iframeWrapper.removeClass('hidden');
+      cardDescription.addClass('hidden');
+    });
+  });
 });
