d238c46b2d51dd4ca1837004de9ab378ad24dfe9
Livien Yin <LivienYin@gmail.com>
2013-05-11 15:24:00 -0700

    contribute icon added. re-routed edit profile link in nav bar

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -3,11 +3,9 @@ $(document).ready(function() {
     var cardDescription = $(this).children('.card_description');
     var iframeWrapper = $(this).children('.iframe_wrapper');
     $(this).mouseover(function () {
-      iframeWrapper.addClass('hidden');
       cardDescription.removeClass('hidden');
     });
     $(this).mouseout(function () {
-      iframeWrapper.removeClass('hidden');
       cardDescription.addClass('hidden');
     });
   });
