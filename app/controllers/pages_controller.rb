829ea9f9f027044a8ef8bc7f26def09c1009c641
Jeffrey Kao <jeffthestampede@gmail.com>
2013-05-13 00:11:29 -0700

    fix front page favorite bug and made limit 24

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,7 +7,7 @@ class PagesController < ApplicationController
       @contents = Content.tagged_with(params[:tag]).order('favorites').reverse
     else
       # @contents = Content.all
-      @contents = Content.limit(15).order('favorites').reverse
+      @contents = Content.order('favorites').limit(24).reverse_order
     end
 
     respond_to do |format|
