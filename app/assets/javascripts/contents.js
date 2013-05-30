7de371f9191d2cf1f0c2ebbc3c796a99f1312a38
Jeffrey Kao <jeffthestampede@gmail.com>
2013-05-30 13:49:18 -0700

    text editor

--- app/assets/javascripts/contents.js
+++ app/assets/javascripts/contents.js
@@ -7,26 +7,14 @@ $(document).ready(function() {
     var css = editor2.getValue();
     var children = $('#iframe-wrapper').children();
     var iframe = $(children[0]);
+    $('#htmloutput').html(html); //.replace(/</g, "%3C").replace(/>/g, "%3E"));
+    $('#cssoutput').html(css);
     iframe.appendTo('#iframe').contents().find('body').html(html);
     iframe.appendTo('#iframe').contents().find('head').html('<style>' + css + '</style>');
-  }
-<<<<<<< HEAD
+  };
 
-  clearInterval(myTimer);
-  myTimer = setInterval(htmlrender, 250);
-
-  $('.submit_btn').click(function() {
-    var html = editor.getValue();
-    var css = editor2.getValue();
-    $('#htmloutput').append(html.replace(/</g, "%3C").replace(/>/g, "%3E"));
-    $('#cssoutput').append(css);
-  });
-
-});
-=======
   htmlrender();
   // Binds callable htmlrender to event 'input propertychange'
   // htmlrender gets called everytime text input changes
   $('.code_field').bind('input propertychange', htmlrender);
 });
->>>>>>> d61a61e7b32941e1a08d116bac91052365341bed
